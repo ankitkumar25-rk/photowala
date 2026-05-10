e415ede75300a7c85dd6059e34132d9420275984
ankitkumar25-rk <ankitgia336@gmail.com>
2026-05-10 15:31:35 +0530

    fixing errors

--- backend/src/app.js
+++ backend/src/app.js
@@ -116,6 +116,7 @@ const sessionOptions = {
   cookie: {
     secure: process.env.NODE_ENV === 'production',
     httpOnly: true,
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   },
   ...(sessionStore ? { store: sessionStore } : {}),
